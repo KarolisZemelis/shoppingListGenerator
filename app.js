6a1be51e0327428b484ff6ef01e39589aa0786e7
KarolisZemelis <karoliszemlis@gmail.com>
2024-12-17 10:03:12 +0200

    Initial setup HTML

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const addIngredientRowTemplate = document.querySelector(
   "[data-form-ingredient]"
 );
 
-function rand(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-}
-
 addIngredientBtn.addEventListener("click", () => {
   const addIngredientRow = addIngredientRowTemplate.content.cloneNode(true);
   recipeForm.appendChild(addIngredientRow);
