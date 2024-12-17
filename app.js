e3e242d6b20775ed9fd57780868abac95dea7bab
KarolisZemelis <karoliszemlis@gmail.com>
2024-12-17 11:01:12 +0200

    started forming recipe array logic from form data

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const addIngredientBtn = document.querySelector("[data-form-addIngredientBtn]");
 const removeIngredientBtn = document.querySelector(
   "[data-form-removeIngredientBtn]"
 );
+const recipeSubmitBtn = document.querySelector(
+  "[data-form-submitIngredientBtn]"
+);
 
 const addIngredientRowTemplate = document.querySelector(
   "[data-form-ingredient]"
 );
+let recipeObject = {};
 
 addIngredientBtn.addEventListener("click", () => {
   const addIngredientRow = addIngredientRowTemplate.content.cloneNode(true);
   recipeForm.appendChild(addIngredientRow);
+});
+
+recipeSubmitBtn.addEventListener("click", (_) => {
+  const recipeName = document.querySelector("[data-form-name]").value;
+  console.log(recipeName);
+  const recipeCalories = document.querySelector("[data-form-calories]").value;
+  console.log(recipeCalories);
+  recipeObject[recipeName] += recipeCalories;
+  console.log(recipeObject);
 });
