04af5192e2c6f5961b059a78504aaf2517ecbfad
KarolisZemelis <karoliszemlis@gmail.com>
2024-12-23 13:51:00 +0200

    started forming recipe render template in html and js

--- public/js/fetchRecipes.js
+++ public/js/fetchRecipes.js
@@ -1,3 +1,5 @@
+const recipeBoxTemplate = document.querySelector("[data-render-recipe]");
+
 async function fetchRecipes() {
   try {
     const response = await fetch("/api/recipes");
@@ -5,12 +7,17 @@ async function fetchRecipes() {
     const recipeList = document.getElementById("recipe-list");
     recipeList.innerHTML = ""; // Clear previous recipes
 
+    const addRecipeBox = recipeBoxTemplate.content.cloneNode(true);
+    recipeList.appendChild(addRecipeBox);
+
     recipes.forEach((recipe) => {
-      const recipeItem = document.createElement("li");
-      recipeItem.textContent = `${
-        recipe.name
-      } - Ingredients: ${recipe.ingredients.join(", ")}`;
-      recipeList.appendChild(recipeItem);
+      for (i = 0; i < recipe.ingredients.length - 1; i++) {
+        for (const [key, value] of Object.entries(recipe.ingredients[i])) {
+          // let ingredient = document.createElement("p");
+          // ingredient.innerText = `${key} : ${value}`;
+          // ingredientContainer.appendChild(ingredient);
+        }
+      }
     });
   } catch (error) {
     console.error("Error fetching recipes:", error);
