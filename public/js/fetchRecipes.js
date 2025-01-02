abd55a317bd1b3983fe61f985d75a1473fafae76
KarolisZemelis <karoliszemlis@gmail.com>
2025-01-02 18:31:56 +0200

    added edit and delete buttons to the template and partial js functionality for edit button

--- public/js/fetchRecipes.js
+++ public/js/fetchRecipes.js
@@ -1,4 +1,26 @@
 const recipeBoxTemplate = document.querySelector("[data-render-recipe]");
+const recipeContainers = document.querySelectorAll("[data-recipe-container]");
+
+const editRecipe = (recipe, name, type, calories) => {
+  name.innerHTML = `<input type="text" name="ingredientName" value=${recipe.name} data-form-ingredientName />`;
+  console.log("jau editinam", recipe.type);
+  type.innerHTML = `
+  <select name="recipeType" id="recipeType" data-form-type>
+    <option value="breakfast" ${
+      recipe.type === "breakfast" ? "selected" : ""
+    }>Pusryčiai</option>
+    <option value="lunch" ${
+      recipe.type === "lunch" ? "selected" : ""
+    }>Pietūs</option>
+    <option value="snack" ${
+      recipe.type === "snack" ? "selected" : ""
+    }>Užkandis</option>
+    <option value="dinner" ${
+      recipe.type === "dinner" ? "selected" : ""
+    }>Vakarienė</option>
+  </select>`;
+  calories.innerHTML = `<input type="number" name="recipeCalories" value=${recipe.calories} data-form-calories />`;
+};
 
 async function fetchRecipes() {
   try {
@@ -18,7 +40,9 @@ async function fetchRecipes() {
       let ingredientList = addRecipeBox.querySelector(
         "[data-ingredient-container]"
       );
-      console.log(recipe);
+      const editButton = addRecipeBox.querySelector("[data-edit-button]");
+      const deleteButton = addRecipeBox.querySelector("[data-delete-button]");
+
       for (i = 0; i <= recipe.ingredients.length - 1; i++) {
         let ingredient = document.createElement("p");
         for (const [key, value] of Object.entries(recipe.ingredients[i])) {
@@ -27,6 +51,25 @@ async function fetchRecipes() {
         ingredientList.appendChild(ingredient);
         recipeList.appendChild(addRecipeBox);
       }
+
+      editButton.addEventListener("click", () => {
+        let recipeCard = editButton.parentElement.parentElement;
+        let name = recipeCard.querySelector("[data-recipe-name]");
+
+        let type = recipeCard.querySelector("[data-recipe-type]");
+        console.log(type);
+        let calories = recipeCard.querySelector("[data-recipe-calories]");
+        let ingredients = recipeCard.querySelector(
+          "[data-ingredient-container]"
+        );
+
+        editRecipe(recipe, name, type, calories, ingredients);
+      });
+
+      deleteButton.addEventListener("click", () => {
+        console.log(`Deleting recipe: ${recipe.name}`);
+        // addedRecipe.remove(); // Example: Remove the recipe from the DOM
+      });
     });
   } catch (error) {
     console.error("Error fetching recipes:", error);
